34044d13fa9d3e036ac1e5b20bf17076e266be95
Shruti Tiwari <sh03shruti@gmail.com>
2021-11-09 22:44:26 +0530

    operational error handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require('./../utils/appError');
+
+const handelCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -37,6 +43,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    error.name = err.name;
+    if (error.name === 'CastError') {
+      error = handelCastErrorDB(error);
+    }
+    sendErrorProd(error, res);
   }
 };
