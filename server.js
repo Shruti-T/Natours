9d98e28aa154045fbbdaf40617a1773c80e950dc
Shruti Tiwari <sh03shruti@gmail.com>
2022-03-15 07:59:07 +0530

    unhandeled exception

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('Unhandeled Exception!!! Shutting Down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './.env' });
 const app = require('./app');
 
@@ -24,9 +30,11 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
   console.log('Unhandeled Rejection!!! Shutting Down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
 });
+
+// console.log(n);
