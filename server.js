5ac82225ddf62aefa7ee6f82786befadb86ce38e
Shruti Tiwari <sh03shruti@gmail.com>
2022-03-15 07:47:42 +0530

    promise rejection

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandeled Rejection!!! Shutting Down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
