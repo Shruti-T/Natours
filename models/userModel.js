b2b353cb821715534ad40b56464a0e44255059bb
Shruti Tiwari <sh03shruti@gmail.com>
2022-03-18 23:07:03 +0530

    modelling users

--- models/userModel.js
+++ models/userModel.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose');
-
+const validator = require('validator');
 //name,email,photo,password,passwordConfirm.
 
 const userModel = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A user must fill his/her name'],
-    trim: true
+    required: [true, 'Please tell us your name.']
   },
   email: {
     type: String,
     unique: true,
-    required: [true, 'A user must have an email']
+    required: [true, 'A user must have an email'],
+    validate: [validator.isEmail, 'Please provide a valid email.']
   },
-  photo: {
-    type: String
-  },
+  photo: String,
   password: {
     type: String,
+    required: [true, 'Please provide a password'],
     minLength: [8, 'A password must have minimum 8 characters!']
   },
-  confirmPassword: {
-    type: String
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password']
   }
 });
 
