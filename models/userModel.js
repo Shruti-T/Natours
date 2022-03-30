4938d3b0bc350fccae7247ded87ca30b674457a3
Shruti Tiwari <sh03shruti@gmail.com>
2022-03-30 23:05:34 +0530

    update

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-//name,email,photo,password,passwordConfirm.
 
 const userModel = new mongoose.Schema({
   name: {
