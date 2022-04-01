71722b843d7c8abaafdc1fb9be6027729652f7c5
Shruti Tiwari <sh03shruti@gmail.com>
2022-04-01 15:44:19 +0530

    signingUpUsers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
@@ -8,8 +9,14 @@ exports.signUp = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
   });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
