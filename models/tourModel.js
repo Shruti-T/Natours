8a1ca1b840559fefbeac5a0a45d977a1203bdaa1
Shruti Tiwari <sh03shruti@gmail.com>
2022-09-13 00:32:48 +0530

    populate tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -151,11 +151,20 @@ tourSchema.pre(/^find/, function(next) {
   this.start = Date.now();
   next();
 });
+tourSchema.pre(/^find/,function(next){
+  this.populate({
+    path:'guides',
+    select:'-__v -passwordChangedAt'
+  });
+  next();
+})
 
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
+
+
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next) {
