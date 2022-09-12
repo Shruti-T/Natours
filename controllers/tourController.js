4157ee3d141f5800ce7981e716f7b732fcea06c0
Shruti Tiwari <sh03shruti@gmail.com>
2022-09-12 23:53:25 +0530

    update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,7 +30,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id);
+  const tour = await Tour.findById(req.params.id).populate('guides');
   // Tour.findOne({ _id: req.params.id })
   if (!tour) {
     return next(new AppError('No tour found with that ID', 404));
