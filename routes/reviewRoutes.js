426e177668a35b14cafa0417e527efbc9dbf0690
Shruti Tiwari <sh03shruti@gmail.com>
2022-09-21 20:52:25 +0530

    mergeParams

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+
+//POST /Tour/id-19301/reviews   (this is the nested route)
+//POST /Tour/id-19301/reviews
+//GET /Tour/id-19301/reviews
+//GET /Tour/id-19301/reviews/reviewId-19301
+//POST /reviews
 
 router
   .route('/')
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
-const reviewController = require('../controllers/reviewController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
 
+router.use('/:tourId/reviews', reviewRouter);
 // router.param('id', tourController.checkID);
-
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
@@ -27,19 +27,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-//POST /Tour/id-19301/reviews
-//GET /Tour/id-19301/reviews
-//GET /Tour/id-19301/reviews/reviewId-19301
-//this is the nested route
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
