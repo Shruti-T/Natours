e556d53c50e4fcf3270078dd1ac5bced6824bb94
Shruti Tiwari <sh03shruti@gmail.com>
2022-10-10 18:00:31 +0530

    performance inc with index

--- models/tourModel.js
+++ models/tourModel.js
@@ -117,6 +117,9 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
