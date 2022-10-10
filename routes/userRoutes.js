8c13400735195802bec5782ce23d2f15c9b0ba03
Shruti Tiwari <sh03shruti@gmail.com>
2022-10-10 12:34:59 +0530

    Auth userRouter

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,23 +6,19 @@ const router = express.Router();
 
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+//router is like a small application and all these are middleware .....
+router.use(authController.protect);
+//Middelware runs in sequence, hence all the routes below this will have 'authController.protect'.....
+router.patch('/updateMyPassword', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
