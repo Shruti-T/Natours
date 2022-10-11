ac54c0b52927a02e0c180afe1b1e1854f8936284
Shruti Tiwari <sh03shruti@gmail.com>
2022-10-11 09:37:21 +0530

    update

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -47,5 +47,27 @@ reviewSchema.pre(/^find/, function(next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function(tour) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  console.log(stats);
+};
+
+reviewSchema.pre('save', function(next) {
+  //this points to current review
+  this.constructor.calcAverageRatings(this.tour);
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
