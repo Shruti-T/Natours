a6fa3328d0de9d0aa1fdef150fd9d436acccd30d
Shruti Tiwari <sh03shruti@gmail.com>
2023-01-09 00:44:07 +0530

    extends in pug

--- app.js
+++ app.js
@@ -73,11 +73,24 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base',{
-    tour:'The Forest Hiker',
-    user:'Shruti'
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Shruti'
   });
 });
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker'
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
