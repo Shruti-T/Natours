7676b91f3822d88e5cc73951b9815687c2594ff3
Shruti Tiwari <sh03shruti@gmail.com>
2023-01-08 23:13:37 +0530

    base header

--- app.js
+++ app.js
@@ -73,7 +73,10 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base',{
+    tour:'The Forest Hiker',
+    user:'Shruti'
+  });
 });
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
