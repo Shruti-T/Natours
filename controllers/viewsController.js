b916a4b28e7610018c72eb7c0bfb706934e0e75b
Shruti Tiwari <sh03shruti@gmail.com>
2023-01-09 21:00:41 +0530

    tour.pug template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,8 +15,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker'
+exports.getTour = catchAsync(async (req, res) => {
+  //1) get the data, for the requested tour(including reviews and guides)
+  const tour = await Tour.findOne({ slug: `${req.params.slug}` }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  //2) Build template
+
+  //3) Render template using data from 1
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+    tour
+  });
+});
