9df2c0c752a3894369ca8a8f8f14aeebac27f27f
Shruti Tiwari <sh03shruti@gmail.com>
2023-01-12 01:15:03 +0530

    bundler

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,7 @@
-const login = async (email, password) => {
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -11,20 +14,13 @@ const login = async (email, password) => {
     });
 
     if (res.data.status == 'success') {
-      alert('Logged in successfully');
+      showAlert('success', 'Logged in successfully');
       window.setTimeout(() => {
         location.assign('/');
       }, 1000);
     }
     console.log(res);
   } catch (error) {
-    alert(error.response.data.message);
+    showAlert('error', error.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', e => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,49 +1,49 @@
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+export const displayMap = locations => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoidGl3YXJpc2hydXRpIiwiYSI6ImNsY3B0YjZybTA2N3kzb2xxcTdmYWNhbXAifQ.I1uqTM4lrwrM-1h4m-TkyA';
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoidGl3YXJpc2hydXRpIiwiYSI6ImNsY3B0YjZybTA2N3kzb2xxcTdmYWNhbXAifQ.I1uqTM4lrwrM-1h4m-TkyA';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/tiwarishruti/clcpwh2r2000f15m0optmjkon',
+    scrollZoom: false
+    //   center: [-118.119398, 34.105497],
+    //   zoom: 8,
+    //   interactive: false
+  });
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/tiwarishruti/clcpwh2r2000f15m0optmjkon',
-  scrollZoom: false
-  //   center: [-118.119398, 34.105497],
-  //   zoom: 8,
-  //   interactive: false
-});
+  const bounds = new mapboxgl.LngLatBounds();
 
-const bounds = new mapboxgl.LngLatBounds();
+  locations.forEach(loc => {
+    //Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-locations.forEach(loc => {
-  //Create marker
-  const el = document.createElement('div');
-  el.className = 'marker';
+    //Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-  //Add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom'
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    //Add popup
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
 
-  //Add popup
-  new mapboxgl.Popup({
-    offset: 30
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-    .addTo(map);
+    //Extend the map bounds to include current locations
+    bounds.extend(loc.coordinates);
+  });
 
-  //Extend the map bounds to include current locations
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100
-  }
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+};
