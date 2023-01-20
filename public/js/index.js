c7644e31ba2cd6042787d1f8b2c6331d95931dd2
Shruti Tiwari <sh03shruti@gmail.com>
2023-01-20 23:26:14 +0530

    update

--- public/js/index.js
+++ public/js/index.js
@@ -40,11 +40,20 @@ if (userDataForm) {
 }
 
 if (userPasswordForm) {
-  userPasswordForm.addEventListener('submit', e => {
+  userPasswordForm.addEventListener('submit', async e => {
     e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
     const passwordCurrent = document.getElementById('password-current').value;
     const password = document.getElementById('password').value;
     const passwordConfirm = document.getElementById('password-confirm').value;
-    updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save Password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
