82aae4ce7b52ac56e79c7dc092ac19c2e051e201
Shruti Tiwari <sh03shruti@gmail.com>
2023-01-24 13:53:32 +0530

    uploading tour images

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,37 @@
+const multer = require('multer');
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  {
+    name: 'images',
+    maxCount: 3
+  }
+]);
+
+// upload.array('images', 5);
+exports.resizeTourImages = (req, res, next) => {
+  console.log(req.files);
+  next();
+};
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
