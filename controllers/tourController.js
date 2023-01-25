32e4ad66daa91a033392a91bd4c86a7f9d9b3de2
Shruti Tiwari <sh03shruti@gmail.com>
2023-01-26 01:18:36 +0530

    tour images upload

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -45,11 +45,11 @@ exports.resizeTourImages = catchAsync(async (req, res, next) => {
   await Promise.all(
     req.files.images.map(async (file, i) => {
       const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
-      await sharp(req.files.imageCover[0].buffer)
+      await sharp(file.buffer)
         .resize(2000, 1333)
         .toFormat('jpeg')
         .jpeg({ quality: 90 })
-        .toFile(`public/img/tours/${req.body.imageCover}`);
+        .toFile(`public/img/tours/${filename}`);
 
       req.body.images.push(filename);
     })
--- controllers/userController.js
+++ controllers/userController.js
@@ -33,7 +33,7 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+exports.resizeUserPhoto = async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
@@ -45,7 +45,7 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-});
+};
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
