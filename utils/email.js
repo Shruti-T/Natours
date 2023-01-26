8d7ad41e57f8976c2ff3e1aa6d0abfa925af0525
Shruti Tiwari <sh03shruti@gmail.com>
2023-01-26 14:35:59 +0530

    email object

--- utils/email.js
+++ utils/email.js
@@ -1,4 +1,60 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Shruti Tiwari <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //sendgrid
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
+
+  async send(template, subject) {
+    //send the actual email
+    //1) render html based on pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template.pug}`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject
+      }
+    );
+
+    //2) define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
+
+    //3) create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sentWelcome() {
+    await this.send('welcome', 'Welcome to the Natours familty!');
+  }
+};
 
 // const sentEmail = option => {
 //   //1) create a transportor
@@ -11,28 +67,3 @@ const nodemailer = require('nodemailer');
 //   });
 //   //activate in gmail "less secure app" option
 // };
-
-const sendEmail = async options => {
-  //1) create a transportor
-  const transportor = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
-    }
-  });
-
-  //2)Define the email options
-  const mailOptions = {
-    from: 'Shruti Tiwari <sh03shruti@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-  };
-
-  //3) actually send the email
-  await transportor.sendMail(mailOptions);
-};
-
-module.exports = sendEmail;
