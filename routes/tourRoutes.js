8495a4f3c00e3eb32ac2f214bbecdfe6481ab1c8
Shruti Tiwari <sh03shruti@gmail.com>
2023-02-02 11:01:23 +0530

    coffee 3rdParty data test

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const axios = require('axios');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
 const reviewRouter = require('../routes/reviewRoutes');
+
+// import fetch from 'node-fetch';
 
 const router = express.Router();
 
@@ -53,9 +56,23 @@ router
     tourController.deleteTour
   );
 
-router.route('/coffeeTest/:coffeeData').get((req, res) => {
+router.route('/coffeeTest/:coffeeData').get(async (req, res) => {
+  let quality = 0;
+  await axios
+    .get(
+      'https://python-public-api-production.up.railway.app/?my_dict={%22aroma%22:8.67,%22Flavor%22:8.83,%22Acidity%22:8.75,%22Body%22:8.5,%22Balance%22:8.42,%22Uniformity%22:10.0,%22Clean.Cup%22:10.0,%22Sweetness%22:10.0,%22Cupper.Points%22:8.75,%22Moisture%22:0.12,%22Quakers%22:0.0,%22Category.One.Defects%22:0.0,%22Category.Two.Defects%22:0,%22altitude_mean_meters%22:2075.0}'
+    )
+    .then(ans => {
+      quality = ans.data;
+      console.log(ans.data);
+    })
+    .catch(err => {
+      console.error(err);
+      quality = 0;
+    });
+
   res.status(200).json({
-    data: `the coffee quality is ${88} %`
+    quality
   });
 });
 
