9b7b6b566bd1347f4a3d4a099c80bdae324b9518
Shruti Tiwari <sh03shruti@gmail.com>
2023-02-19 20:07:40 +0530

    mailtrap issue resolved

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,14 @@ module.exports = class Email {
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       //sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      });
     }
 
     return nodemailer.createTransport({
